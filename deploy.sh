6ab0513a063e682c226a483b80e033ff12a22f9b
Bing <bxu7@buffalo.edu>
2021-09-30 23:34:52 -0400

    added travis yaml file and deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t johnywind2015/multi-client:latest -t johnywind2015/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t johnywind2015/multi-server:latest -t johnywind2015/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t johnywind2015/multi-worker:latest -t johnywind2015/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push johnywind2015/multi-client:latest
+docker push johnywind2015/multi-server:latest
+docker push johnywind2015/multi-worker:latest
+
+docker push johnywind2015/multi-client:$SHA
+docker push johnywind2015/multi-server:$SHA
+docker push johnywind2015/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment server=johnywind2015/multi-client:$SHA
+kubectl set image deployments/server-deployment server=johnywind2015/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=johnywind2015/multi-worker:$SHA
